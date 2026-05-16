9607ac8e27aa56777c999df7d95844e7c1fa39b3
Aditya Khariwal <storageman9414@gmail.com>
2026-05-16 15:25:00 +0530

    Built a code for coffee machine with some graphics although this should have been done in class

---
+++ Day 15 - Coffee Machine/coffee machine.py
@@ -0,0 +1,199 @@
+title = r'''
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠈⢻⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠙⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧⠀⠀⠘⢿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡆⠀⠀⠘⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⠃⠀⠀⠀⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠃⠀⠀⠀⠀⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡏⣀⣀⣀⠀⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣀⡤⠤⠒⠒⠋⠉⠉⠻⣧⠀⠀⠀⠈⠉⠁⠀⠀⠀⠢⢄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣾⣿⠀⠀⠀⠀⣀⣀⣀⣀⣤⣽⣦⣄⣀⣀⣀⣀⠀⠀⠀⠀⢹⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⣿⣿⠷⠾⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠶⠚⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢿⣿⡏⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⣸⠛⠻⣷⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠸⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠃⠀⢠⣿⠇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣹⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣎⣠⣴⠿⠃⠀⠀⠀
+⠀⢀⣠⠔⠒⠈⠉⠀⠹⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠾⠛⠛⠉⠒⠢⣄⠀⠀
+⠀⣿⡁⠀⠀⠀⠀⠀⠀⠈⢻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣾⡃⠀⠀⠀⠀⠀⠀⠀⡟⠀
+⠀⠙⠻⣶⣀⠀⠀⠀⠀⠀⠀⠈⠙⠲⠦⣤⣄⣀⣀⣀⣤⣤⣾⣯⡵⠞⠋⠀⠀⠀⣀⠟⠀⠀⠀⠀
+⠀⠀⠀⠀⠉⠛⠻⠿⠿⠶⠶⠤⠤⠤⣄⣀⣀⣀⣀⣀⣀⣀⣀⡠⠤⠤⠤⠴⠖⠉⠀⠀⠀⠀⠀⠀
+'''
+
+print(title)
+print("Welcome to the coffee machine!")
+
+graphics = {    
+    "report" : '''
+    ┌─────────────────────────────┐
+    │          Report             │
+    └─────────────────────────────┘
+    ''',
+    "menu" : '''
+    ┌─────────────────────────────┐
+    │           Menu              │
+    └─────────────────────────────┘
+    ''',
+    "espresso" : '''
+    ┌─────────────────────────────┐
+    │          Espresso           │
+    └─────────────────────────────┘
+    ''',
+
+    "latte" : '''
+    ┌─────────────────────────────┐
+    │           Latte             │
+    └─────────────────────────────┘
+    ''',
+    "cappuccino" : '''
+    ┌─────────────────────────────┐
+    │         Cappuccino          │
+    └─────────────────────────────┘
+    ''',
+    "hot_water" : '''
+    ┌─────────────────────────────┐
+    │          Hot Water          │
+    └─────────────────────────────┘
+    '''
+}
+
+water = 400
+milk = 540
+coffee = 120
+money = 0
+
+menu = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+            "milk": 0,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    },
+    "hot water": {
+        "ingredients": {
+            "water": 300,
+            "milk": 0,
+            "coffee": 0,
+        },
+        "cost": 0.5,
+    }
+}
+currency = {
+    "quarters": 0.25,
+    "dimes": 0.10,
+    "nickels": 0.05,
+    "pennies": 0.01
+}
+
+def print_report():
+    print(f"Water: {water}ml")
+    print(f"Milk: {milk}ml")
+    print(f"Coffee: {coffee}g")
+    print(f"Money: ${money}")
+    
+def print_menu():
+    print("Menu:")
+    for item in menu:
+        if (milk >= menu[item]["ingredients"]["milk"] and 
+            water >= menu[item]["ingredients"]["water"] and 
+            coffee >= menu[item]["ingredients"]["coffee"]):
+            print(f"{item}: ${menu[item]['cost']}")
+        else:
+            print(f"{item}: ${menu[item]['cost']} (Out of stock)")
+    
+    print(graphics["espresso"])
+    print(graphics["latte"])
+    print(graphics["cappuccino"])
+    print(graphics["hot_water"])
+   
+def make_coffee(drink):
+    global water, milk, coffee, money        
+    water -= menu[drink]["ingredients"]["water"]
+    milk -= menu[drink]["ingredients"]["milk"]
+    coffee -= menu[drink]["ingredients"]["coffee"]
+    money += menu[drink]["cost"]
+    print(f"Here is your {drink}. Enjoy!")
+        
+def process_coins(amount):
+    print("Please insert coins.")
+    quarters = int(input("Please insert quarters? "))
+    dimes = int(input("Please insert dimes? "))
+    nickels = int(input("Please insert nickels? "))
+    pennies = int(input("Please insert pennies? "))
+    
+    total = (quarters * currency["quarters"] + 
+             dimes * currency["dimes"] + 
+             nickels * currency["nickels"] + 
+             pennies * currency["pennies"])
+    
+    if total >= amount:
+        change = round(total - amount, 2)
+        if change > 0:
+            print(f"Here is ${change} in change.")
+        return True
+    else:
+        print("Sorry, that's not enough money. Money refunded.")
+        return False
+
+def fill_resources():
+    global water, milk, coffee
+    water += int(input("How much water would you like to add? "))
+    milk += int(input("How much milk would you like to add? "))
+    coffee += int(input("How much coffee would you like to add? "))
+    print("Resources filled.")
+    
+is_on = True
+while is_on:
+    print("Please select an action: ")
+    print(graphics["report"])
+    print(graphics["menu"])
+    
+    action = input("Type 'report' to view resources or 'menu' to view menu: ").lower()
+    if action == "report":
+        print_report()
+    elif action == "menu":
+        print_menu()
+        choice = input("What would you like to order? ").lower()
+        if choice in menu:
+            
+            if (menu[choice]["ingredients"]["milk"] > milk or 
+                menu[choice]["ingredients"]["water"] > water or
+                menu[choice]["ingredients"]["coffee"] > coffee):
+                print("Sorry, out of stock.")
+                continue
+                
+            elif process_coins(menu[choice]["cost"]):
+                make_coffee(choice)
+                
+            else:
+                print("Transaction failed.")
+                
+    elif action == "off":
+        is_on = False
+    elif action == "fill":
+        fill_resources()
+    else:
+        print("Invalid action.")
